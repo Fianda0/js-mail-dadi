17be7ab728a8afd00a99834f8f74289bde4b80d9
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-22 16:04:18 +0200

    completamento risultato mail

--- Mail/mail.js
+++ Mail/mail.js
@@ -16,13 +16,23 @@ console.log(mailInput.value)
 
 btnControl.addEventListener('click', function () {
     console.log(mailInput.value)
-
+    let n = 0
     for (i = 0; i < ingresso.length; i++) {
         console.log(ingresso[i])
 
         if (mailInput.value == ingresso[i]) {
-            console.log('entra')
+            document.getElementById('risultato').innerText = 'Puoi entrare'
+            console.log('si')
+        } else {
+            n = n + 1
+            console.log('no')
         }
-    }
+        console.log('n=', n)
+        console.log('array', ingresso.length)
 
+    }
+    if (n == ingresso.length) {
+        document.getElementById('risultato').innerText = 'Non puoi entrare'
+        console.log('non puoi entrare')
+    }
 })
